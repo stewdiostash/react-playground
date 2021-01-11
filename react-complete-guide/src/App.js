d2a2ea60c941a97c33fbc84493d70753bbb8064e
Andrew Stewart <1andrewstewart@gmail.com>
2021-01-11 10:52:30 -0500

    conditional code moved out of main div

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -49,32 +49,38 @@ class App extends Component {
       borderRadius: "6px",
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, "Bart")}
+            changed={this.nameChangeHandler}
+          />
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          >
+            Chess Master
+          </Person>
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Name list</h1>
         <button style={style} onClick={this.togglePersonsHandler}>
           {this.state.showPersons ? "Hide Names" : "Show Names"}
         </button>
-        {this.state.showPersons ? (
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, "Bart")}
-              changed={this.nameChangeHandler}
-            />
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-            >
-              Chess Master
-            </Person>
-          </div>
-        ) : null}
+        {persons}
       </div>
     );
     // return React.createElement(
