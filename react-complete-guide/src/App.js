2fa81090a751aadc1662645d782ca48745e9b981
Andrew Stewart <1andrewstewart@gmail.com>
2021-01-25 09:09:21 -0500

    added radium package, hover styles

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Radium from "radium";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -55,6 +56,10 @@ class App extends Component {
       padding: "12px 16px",
       cursor: "pointer",
       borderRadius: "6px",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black",
+      },
     };
 
     let persons = null;
@@ -76,6 +81,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = "red";
+      style[":hover"] = {
+        backgroundColor: "salmon",
+        color: "black",
+      };
     }
 
     let classes = [];
@@ -105,4 +114,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
